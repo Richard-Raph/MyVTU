ecc7ce27f91f84b14a306c8aba77d33ba17afd12
Richard-Raph <richardakpan77@gmail.com>
2024-06-29 11:36:33 +0330

    added variation route for mtn data

--- server/server.js
+++ server/server.js
@@ -1,17 +1,33 @@
 require('dotenv').config()
 
-const { configDotenv } = require('dotenv');
 // import libraries
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
+const port = process.env.PORT || 6000
 
 app.use(express.json());
+app.use(bodyParser.json());
 
-port = process.env.PORT
 
 // Connect to MongoDB database using Mongoose
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("Connected to the Database and running on port " + port))
     .catch((err) => console.error(`Error connecting to MongoDB: ${err}`));
+
+
+// Import routes
+const variationsRoute = require('./routes/airtime/mtn/variations');
+// const purchaseRoute = require('./routes/airtime/mtn/purchase');
+// const queryStatusRoute = require('./routes/airtime/mtn/queryStatus');
+
+// Use routes
+app.use('/api/variations', variationsRoute);
+// app.use('/api/purchase', purchaseRoute);
+// app.use('/api/query-status', queryStatusRoute);
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
